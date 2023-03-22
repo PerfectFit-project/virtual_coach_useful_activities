2a2fda36ee4b80d0341d22c4b6556a55a6b85729
Nele Albers <nalbers1@tudelft.nl>
2023-03-22 09:58:36 +0100

    try out https

--- frontend/server.js
+++ frontend/server.js
@@ -1,7 +1,23 @@
-var express= require('express');
+// Based on this post: https://adamtheautomator.com/https-nodejs/
+
+// Import the express module
+var express = require('express');
+
+// Instantiate an Express application
 const app = express();
 var path = require('path')
-const server = require('http').createServer(app);
+
+// Create listener on port 3000 that points to the Express app
+const https = require('https');
+const server = https.createServer(
+		// Provide the private and public key to the server by reading each
+		// file's content with the readFileSync() method.
+    {
+      key: fs.readFileSync("key.pem"),
+      cert: fs.readFileSync("cert.pem"),
+    },
+    app
+  )
 var cors = require('cors')
 const PORT = 3000;
 server.listen(PORT);
@@ -10,6 +26,8 @@ console.log(`Server is running on port ${PORT}`);
 app.use(cors())
 app.use(express.static(path.join(__dirname, 'static')));
 
+// This code tells the service to listen to any request coming to the / route.
+// Once the request is received, the line res.sendFile() is executed.
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
  });
