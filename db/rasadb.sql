652d6892cea05fd9a2fca85fa008682d454d70db
Nele Albers <nele.96@web.de>
2023-04-12 14:42:47 +0200

    mysql logs

--- db/rasadb.sql
+++ db/rasadb.sql
@@ -2,6 +2,9 @@ use db;
 
 CREATE TABLE users(prolific_id TEXT, name TEXT, time DATETIME);
 
-
 CREATE TABLE sessiondata(prolific_id TEXT, session_num TEXT, response_type TEXT,
-response_value TEXT, time DATETIME)
+response_value TEXT, time DATETIME);
+
+SET global general_log = 1;
+SET global general_log_file='/var/lib/mysql/mysql.log';
+SET global log_output = 'file'; 
