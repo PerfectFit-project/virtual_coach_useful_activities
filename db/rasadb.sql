b51990d5aad9e0e5e603b5616310bdb55153cffe
Nele Albers <nele.96@web.de>
2023-06-09 13:40:41 +0100

    add auto-increment primary key to mysql db

--- db/rasadb.sql
+++ db/rasadb.sql
@@ -1,8 +1,8 @@
 use db;
 
-CREATE TABLE users(prolific_id TEXT, name TEXT, time DATETIME);
+CREATE TABLE users(idx INT NOT NULL AUTO_INCREMENT, prolific_id TEXT, name TEXT, time DATETIME);
 
-CREATE TABLE sessiondata(prolific_id TEXT, session_num TEXT, response_type TEXT,
+CREATE TABLE sessiondata(idx INT NOT NULL AUTO_INCREMENT, prolific_id TEXT, session_num TEXT, response_type TEXT,
 response_value TEXT, time DATETIME);
 
 SET global general_log = 1;
