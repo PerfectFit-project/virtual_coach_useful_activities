0d784e179f6be76e7d56f452675ca6be3e65373f
Nele Albers <nele.96@web.de>
2023-07-19 15:45:20 +0200

    remove logging db

--- db/rasadb.sql
+++ db/rasadb.sql
@@ -4,7 +4,3 @@ CREATE TABLE users(idx INT NOT NULL AUTO_INCREMENT, prolific_id TEXT, name TEXT,
 
 CREATE TABLE sessiondata(idx INT NOT NULL AUTO_INCREMENT, prolific_id TEXT, session_num TEXT, response_type TEXT,
 response_value TEXT, time DATETIME);
-
-SET global general_log = 1;
-SET global general_log_file='/var/log/mysql/mysql.log';
-SET global log_output = 'file'; 
