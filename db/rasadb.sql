f391647b3a01078e23c4c60a3de24b37de785e2d
Nele Albers <nele.96@web.de>
2023-07-19 16:56:32 +0200

    fix typo db init

--- db/rasadb.sql
+++ db/rasadb.sql
@@ -1,6 +1,6 @@
 use db;
 
-CREATE TABLE users(idx INT NOT NULL AUTO_INCREMENT, prolific_id TEXT, name TEXT, time DATETIME, CONSTRAINT users_pk PRIMARY KEY (idx)));
+CREATE TABLE users(idx INT NOT NULL AUTO_INCREMENT, prolific_id TEXT, name TEXT, time DATETIME, CONSTRAINT users_pk PRIMARY KEY (idx));
 
 CREATE TABLE sessiondata(idx INT NOT NULL AUTO_INCREMENT, prolific_id TEXT, session_num TEXT, response_type TEXT,
 response_value TEXT, time DATETIME, CONSTRAINT sessiondata_pk PRIMARY KEY (idx));
